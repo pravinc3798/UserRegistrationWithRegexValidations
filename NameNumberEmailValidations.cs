0ce5864a6c7eef0927374512ae0f7c1ac9e99aec
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-30 19:27:36 +0530

    Added NameNumberEmailValidations class

---
+++ NameNumberEmailValidations.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationWithRegexValidations
+{
+    public class NameNumberEmailValidations
+    {
+        public Regex NameValidation = new Regex("^[A-Z]{1}[a-z A-Z]{2,}$");
+        public Regex NumberValidation = new Regex("^[0-9]{2}\\s[0-9]{10}$");
+        public Regex EmailValidation = new Regex("^[a-z A-Z 0-9 .+_-]+@[a-z A-Z 0-9 -.]+[.][a-z A-Z 0-9 -]{2,}$");
+
+        public void Check(Regex validation, string name)
+        {
+            var result = (validation.IsMatch(name)) ? " ! Test Passed For : " : " X Test Failed For : ";
+            Console.WriteLine("{0} {1}",result,name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace UserRegistrationWithRegexValidations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameNumberEmail = new NameNumberEmailValidations();
+            var pass = new PasswordValidation();
+
+            Console.Write("\n 1. Check Name (Start with Caps and has minimum 3 Characters) \n 2. Check Number (91 1234567890)(2 digits space 10 digits) \n 3. Email Check (auto checks the list of emails provided in the document) \n 4. Password Check \n 5. Exit \n\n >>>> Enter desired input : "); var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    {
+                        Console.Write("\n Enter a NAME to check : "); var name = Console.ReadLine();
+                        nameNumberEmail.Check(nameNumberEmail.NameValidation, name);
+                        Main(args);
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.Write("\n Enter a NUMBER to check : "); var number = Console.ReadLine();
+                        nameNumberEmail.Check(nameNumberEmail.NumberValidation, number);
+                        Main(args);
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine();
+                        var emails = "abc@yahoo.com,abc-100@yahoo.com,abc.100@yahoo.com,abc111@abc.com,abc-100@abc.net,abc.100@abc.com.au,abc@1.com,abc@gmail.com.com,abc+100@gmail.com";
+                        var emailList = emails.Split(',');
+                        foreach (var email in emailList)
+                            nameNumberEmail.Check(nameNumberEmail.EmailValidation, email);
+                        Main(args);
+                        break;
+                    }
+                case "4":
+                    {
+                        Console.Write("\n 1. Any password with length >8 will be accepted  \n 2. Length>8 and Atleast One Uppercase Character \n 3. Length>8 and Atleast One Uppercase and Atleast One number \n 4. Atmost One special character and Length>8 and Atleast One Uppercase and Atleast One number \n 5. Back \n\n >>>> Enter desired security level : "); var securityLevel = Console.ReadLine();
+                        Console.Write("\n >>>>>> Enter password to check : "); var password = Console.ReadLine();
+
+                        switch (securityLevel)
+                        {
+                            case "1": pass.Check(pass.MinimumLength8, password); Main(args); break;
+                            case "2": pass.Check(pass.OneUpperAndL8, password); Main(args); break;
+                            case "3": pass.Check(pass.OneNumbericOneUL8, password); Main(args); break;
+                            case "4": pass.Check(pass.ExactlyOneSpecialCharacter, password); Main(args); break;
+                            case "5": Main(args); break;
+                            default: Console.WriteLine("\n !! Invalid Input !! \n"); Main(args); break;
+                        }
+
+                        break;
+                    }
+                case "5":
+                    {
+                        break;
+                    }
+                default: Console.WriteLine("\n !! Invalid Input !! \n"); Main(args); break;
+
+            }
+        }
+    }
+}
