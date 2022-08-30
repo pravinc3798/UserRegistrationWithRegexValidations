2858bb4b5c966399be6401f1c84cd58f5f3fc664
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-30 19:27:43 +0530

    Added PasswordValidation class

---
+++ PasswordValidation.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationWithRegexValidations
+{
+    public class PasswordValidation
+    {
+        public Regex MinimumLength8 = new Regex("^(.{,7})$");
+        public Regex OneUpperAndL8 = new Regex("^(.{,7}|[^A-Z]*)$"); 
+        public Regex OneNumbericOneUL8 = new Regex("^(.{,7}|[^A-Z]*|[^0-9]*)$");
+        public Regex ExactlyOneSpecialCharacter = new Regex("^(.{,7}|[^A-Z]*|[^0-9]*|[^.]*\\W{2,}[^.]*|[^\\W]+)$");
+
+        public void Check(Regex validation, string pass)
+        {
+            var result = (!validation.IsMatch(pass)) ? " ! Test Passed For : " : " X Test Failed For : ";
+            Console.WriteLine("{0} {1}", result, pass);
+        }
+    }
+}
